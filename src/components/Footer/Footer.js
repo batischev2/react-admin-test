738a362ec1ff02a5abfe21e54df38daaa51fe40d
Mikola <ko1907@mail.ru>
2023-11-13 17:53:52 +0300

    adaptive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import "./footer.scss";
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="container">
+            <div className="container footer__wrapper">
                 <div className="footer__info">
                     <div className="footer__infoTop">
                         <p>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NavigationItem from "../NavigationItem/NavigationItem";
 import SocialItem from "../SocialItem/SocialItem";
@@ -7,6 +7,8 @@ import instagram from "../../assets/images/socials/instagram.png";
 import whatsapp from "../../assets/images/socials/whatsapp.png";
 import faceBook from "../../assets/images/socials/facebook.png";
 import profile from "../../assets/images/profile-user.png";
+import burger from "../../assets/images/burger-bar.png";
+import close from "../../assets/images/closeBurger.png"
 
 import "./header.scss";
 
@@ -29,6 +31,7 @@ const Header = () => {
                 href: "#"
             },
         ]
+    const [burgerOpen, setBurgerOpen] = useState(false)
     return (
         <header className="header">
             <div className="container">
@@ -49,6 +52,24 @@ const Header = () => {
                         <img src={profile} alt="profile" />
                     </div>
                 </div>
+                <div className="header__burger" onClick={() => setBurgerOpen(!burgerOpen)}>
+                    {burgerOpen ? <img className='burgerClose' src={close} alt='close'/> : <img className='burgerOpen' src={burger} alt='burger' /> }
+                </div>
+                {burgerOpen &&
+                    <div className='header__dropDown'>
+                        <nav className="header__dropDownNavMenu">
+                            {navigationItems.map((item) => {
+                                return <NavigationItem title={item} key={item} />
+                            })}
+                        </nav>
+                        <div className="header__infoSocials">
+                            {socials.map((social) => {
+                                return <SocialItem href={social.href} img={social.img} key={social.name} />
+                            })}
+                        </div>
+                        <span className="header__infoPhone">+7 (861) 111-11-11</span>
+                    </div>
+                }
             </div>
         </header>
     );
