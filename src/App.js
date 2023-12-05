d439c8e9b3acf96bf43b6bb364c604ea0b12c4e8
M1kola <ko1907@mail.ru>
2023-12-05 19:56:40 +0300

    router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate
+} from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -9,7 +16,13 @@ function App() {
     <div className="App">
         <Header />
         <div className='container'>
-            <Home />
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/auth" element={<div>123</div>} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </Router>
         </div>
         <Footer />
     </div>
